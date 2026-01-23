b836c2cd91752efe820cf2b865344c0adb72de17
alvaroorguezz <alvaroo.rguezz@gmail.com>
2026-01-23 22:26:51 +0100

    sevenseg_dec.vhd

--- sevenseg_dec.vhd
+++ sevenseg_dec.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity sevenseg_hex4 is
+entity sevenseg_dec13 is
   generic(
     CLK_HZ      : integer := 50_000_000;
     REFRESH_HZ  : integer := 1000;
@@ -11,116 +11,118 @@ entity sevenseg_hex4 is
   port(
     clk      : in  std_logic;
     reset    : in  std_logic;
-
-    hex_in   : in  std_logic_vector(15 downto 0); -- 4 nibbles
-
-    seg      : out std_logic_vector(6 downto 0);  -- a..g
+    value_in : in  std_logic_vector(12 downto 0);
+    seg      : out std_logic_vector(6 downto 0);
     dp       : out std_logic;
     an       : out std_logic_vector(3 downto 0)
   );
 end entity;
 
-architecture rtl of sevenseg_hex4 is
+architecture rtl_dec of sevenseg_dec13 is
   constant DIV_TICKS : integer := CLK_HZ / (REFRESH_HZ * 4);
-  signal div_cnt     : integer range 0 to DIV_TICKS-1 := 0;
-  signal tick        : std_logic := '0';
-  signal digit_sel   : unsigned(1 downto 0) := (others => '0');
 
-  -- segmentos activos en '1' (luego se invierte si ACTIVE_LOW)
-  function hex_to_7seg(n : std_logic_vector(3 downto 0)) return std_logic_vector is
+  signal div_cnt   : integer range 0 to DIV_TICKS-1 := 0;
+  signal tick      : std_logic := '0';
+  signal digit_sel : unsigned(1 downto 0) := (others => '0');
+
+  signal bcd_reg : unsigned(15 downto 0) := (others => '0');
+
+  function dec_to_7seg_i(n : integer) return std_logic_vector is
     variable s : std_logic_vector(6 downto 0);
   begin
     case n is
-      when "0000" => s := "1111110"; -- 0
-      when "0001" => s := "0110000"; -- 1
-      when "0010" => s := "1101101"; -- 2
-      when "0011" => s := "1111001"; -- 3
-      when "0100" => s := "0110011"; -- 4
-      when "0101" => s := "1011011"; -- 5
-      when "0110" => s := "1011111"; -- 6
-      when "0111" => s := "1110000"; -- 7
-      when "1000" => s := "1111111"; -- 8
-      when "1001" => s := "1111011"; -- 9
-      when "1010" => s := "1110111"; -- A
-      when "1011" => s := "0011111"; -- b
-      when "1100" => s := "1001110"; -- C
-      when "1101" => s := "0111101"; -- d
-      when "1110" => s := "1001111"; -- E
-      when others => s := "1000111"; -- F
+      when 0 => s := "1111110";
+      when 1 => s := "0110000";
+      when 2 => s := "1101101";
+      when 3 => s := "1111001";
+      when 4 => s := "0110011";
+      when 5 => s := "1011011";
+      when 6 => s := "1011111";
+      when 7 => s := "1110000";
+      when 8 => s := "1111111";
+      when 9 => s := "1111011";
+      when others => s := "0000001";
     end case;
     return s;
   end function;
 
-  signal seg_raw : std_logic_vector(6 downto 0);
-  signal an_raw  : std_logic_vector(3 downto 0);
-  signal nibble  : std_logic_vector(3 downto 0);
+  signal seg_o : std_logic_vector(6 downto 0) := (others => '0');
+  signal an_o  : std_logic_vector(3 downto 0) := (others => '1');
 
 begin
 
-  -- tick multiplex
+  gen_dp_low_dec : if ACTIVE_LOW generate
+    dp <= '1';
+  end generate;
+  gen_dp_high_dec : if (not ACTIVE_LOW) generate
+    dp <= '0';
+  end generate;
+
   process(clk)
   begin
     if rising_edge(clk) then
       if reset = '1' then
-        div_cnt <= 0; tick <= '0';
+        div_cnt   <= 0;
+        tick      <= '0';
+        digit_sel <= (others => '0');
       else
+        tick <= '0';
         if div_cnt = DIV_TICKS-1 then
-          div_cnt <= 0; tick <= '1';
+          div_cnt   <= 0;
+          tick      <= '1';
+          digit_sel <= digit_sel + 1;
         else
-          div_cnt <= div_cnt + 1; tick <= '0';
+          div_cnt <= div_cnt + 1;
         end if;
       end if;
     end if;
   end process;
 
-  process(clk)
+  process(value_in)
+    variable bcd : unsigned(15 downto 0);
+    variable bin : unsigned(12 downto 0);
+    variable i   : integer;
   begin
-    if rising_edge(clk) then
-      if reset = '1' then
-        digit_sel <= (others => '0');
-      elsif tick = '1' then
-        digit_sel <= digit_sel + 1;
-      end if;
-    end if;
+    bin := unsigned(value_in);
+    bcd := (others => '0');
+
+    for i in 12 downto 0 loop
+      if bcd(3 downto 0)  >= 5 then bcd(3 downto 0)  := bcd(3 downto 0)  + 3; end if;
+      if bcd(7 downto 4)  >= 5 then bcd(7 downto 4)  := bcd(7 downto 4)  + 3; end if;
+      if bcd(11 downto 8) >= 5 then bcd(11 downto 8) := bcd(11 downto 8) + 3; end if;
+      if bcd(15 downto 12)>= 5 then bcd(15 downto 12):= bcd(15 downto 12)+ 3; end if;
+
+      bcd := bcd(14 downto 0) & bin(i);
+    end loop;
+
+    bcd_reg <= bcd;
   end process;
 
-  -- seleccionar dgito (an0=LS digit normalmente)
-  process(digit_sel, hex_in)
+  process(digit_sel, bcd_reg)
+    variable dig : integer range 0 to 15;
+    variable an_t : std_logic_vector(3 downto 0);
+    variable seg_t: std_logic_vector(6 downto 0);
   begin
-    an_raw  <= "0000";
-    nibble  <= "0000";
-
+    an_t := "0000";
     case digit_sel is
-      when "00" =>
-        an_raw <= "0001";                -- dig 0 (derecha)
-        nibble <= hex_in(3 downto 0);
-      when "01" =>
-        an_raw <= "0010";
-        nibble <= hex_in(7 downto 4);
-      when "10" =>
-        an_raw <= "0100";
-        nibble <= hex_in(11 downto 8);
-      when others =>
-        an_raw <= "1000";                -- dig 3 (izquierda)
-        nibble <= hex_in(15 downto 12);
+      when "00" => dig := to_integer(bcd_reg(3 downto 0));    an_t := "0001";
+      when "01" => dig := to_integer(bcd_reg(7 downto 4));    an_t := "0010";
+      when "10" => dig := to_integer(bcd_reg(11 downto 8));   an_t := "0100";
+      when others => dig := to_integer(bcd_reg(15 downto 12)); an_t := "1000";
     end case;
 
-    seg_raw <= hex_to_7seg(nibble);
-  end process;
+    seg_t := dec_to_7seg_i(dig);
 
-  -- polaridad
-  process(seg_raw, an_raw)
-  begin
     if ACTIVE_LOW then
-      seg <= not seg_raw;
-      an  <= not an_raw;
-      dp  <= '1'; -- apagado
+      seg_o <= not seg_t;
+      an_o  <= not an_t;
     else
-      seg <= seg_raw;
-      an  <= an_raw;
-      dp  <= '0';
+      seg_o <= seg_t;
+      an_o  <= an_t;
     end if;
   end process;
 
-end architecture;
+  seg <= seg_o;
+  an  <= an_o;
 
+end architecture;
